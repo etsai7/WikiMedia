85a8195976c07711f23576c79c64ba25be453e34
Eric Tsai <e.tsai70@gmail.com>
2023-11-12 22:48:46 -0500

    Adding kafka-producer-wikimedia sub-module and configuring it as a Spring Application

---
+++ wikimedia/wikimedia/kafka-producer-wikimedia/src/main/java/com/springboot/wikimedia/WikiMediaProducerApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiMediaProducerApplication {
+    public static void main(String [] args){
+        SpringApplication.run(WikiMediaProducerApplication.class);
+    }
+}
