987f86f9eaa62e3b805b3700b555d7880e3900bd
Eric Tsai <e.tsai70@gmail.com>
2023-11-15 21:49:18 -0500

    Adding docker-compose file and general sql script

---
+++ wikimedia/wikimedia/src/main/resources/sql/db-create.sql
@@ -0,0 +1,8 @@
+create database "wikimedia-data";
+
+# If using IntelliJ, connect first to the db you want, by right-clicking -> new -> query console
+# Then run the following script if wanting to create a new table
+CREATE TABLE test (
+       column1 INT,
+       column2 VARCHAR(50)
+);
