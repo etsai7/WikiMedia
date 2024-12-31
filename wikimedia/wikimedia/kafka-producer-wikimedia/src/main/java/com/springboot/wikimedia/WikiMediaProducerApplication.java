89e1fcc4bebd9151c28c4bb3c5d9a6d933e0ccc2
etsai7 <e.tsai70@gmail.com>
2024-12-31 13:55:54 -0500

    Configuring Producer to send message to broker on receiving msg from the EventHandler

--- wikimedia/wikimedia/kafka-producer-wikimedia/src/main/java/com/springboot/wikimedia/WikiMediaProducerApplication.java
+++ wikimedia/wikimedia/kafka-producer-wikimedia/src/main/java/com/springboot/wikimedia/WikiMediaProducerApplication.java
@@ -1,11 +1,22 @@
 package com.springboot.wikimedia;
 
+import com.springboot.wikimedia.service.WikiMediaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WikiMediaProducerApplication {
+public class WikiMediaProducerApplication implements CommandLineRunner {
+    @Autowired
+    private WikiMediaProducerService wikiMediaProducerService;
+
     public static void main(String [] args){
         SpringApplication.run(WikiMediaProducerApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikiMediaProducerService.sendMessage();
     }
 }
